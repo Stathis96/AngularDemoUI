2753033e460da58cd8b5f6bb76cdf4350b3d014d
StathisIoannidis <ioannidis.stathis@yahoo.gr>
2025-06-24 14:04:16 +0100

    fix on past GTtest

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
-//github test comment
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+
+// This is the entry point for the application.
+bootstrapApplication(AppComponent, appConfig).catch((err) =>
+  console.error(err)
+);
